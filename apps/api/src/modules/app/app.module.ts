d663c6f8f22672814ff69dd7f32ac5d4b8fd24a4
eduardoteles17 <edu.teles@pm.me>
2022-08-23 13:31:59 -0300

    chore(api): remove app service and controller

--- apps/api/src/modules/app/app.module.ts
+++ apps/api/src/modules/app/app.module.ts
@@ -1,11 +1,6 @@
-import { Module } from "@nestjs/common";
-
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
