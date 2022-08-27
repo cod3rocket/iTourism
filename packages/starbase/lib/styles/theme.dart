33a9c08a0f6fcf66bbf991773fd15f69d16e1af4
eduardoteles17 <edu.teles@pm.me>
2022-08-27 09:50:44 -0300

    feat(starbase): add starbase package

---
+++ packages/starbase/lib/styles/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class StarbaseTheme {
+  static const primaryColor = Color.fromRGBO(23, 100, 163, 1);
+
+  static ThemeData get light {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: primaryColor,
+    );
+  }
+
+  static ThemeData get dark {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: primaryColor,
+      brightness: Brightness.dark,
+    );
+  }
+}
