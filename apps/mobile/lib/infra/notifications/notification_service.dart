7c127923b15465bfd104ece89719c63c0669ea2f
Eduardo Teles <edu.teles@pm.me>
2022-08-28 12:35:17 -0300

    feat(mobile): setup local notification service

---
+++ apps/mobile/lib/infra/notifications/notification_service.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'models/send_notificaton_dto.dart';
+
+class NotificationService {
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  final AndroidInitializationSettings _androidInitializationSettings =
+      const AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  NotificationService() {
+    _intNotificationService();
+  }
+
+  _intNotificationService() async {
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: _androidInitializationSettings,
+    );
+
+    await _flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+  }
+
+  sendNotification(SendNotificationModel notificationDto) async {
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      notificationDto.title,
+      notificationDto.topic,
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+
+    await _flutterLocalNotificationsPlugin.show(
+      notificationDto.id!,
+      notificationDto.title,
+      notificationDto.body,
+      notificationDetails,
+    );
+  }
+}
--- apps/mobile/lib/modules/app/app_module.dart
+++ apps/mobile/lib/modules/app/app_module.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:itourism_mobile/infra/notifications/notification_service.dart';
 import 'package:itourism_mobile/modules/home/home_module.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [Bind.lazySingleton((i) => NotificationService())];
   @override
   List<ModularRoute> get routes => [
         ModuleRoute('/home', module: HomeModule()),
