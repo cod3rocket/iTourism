cbe3441e446d136a6afdbb897ad198c9e9a1d876
Henrique GC <henriq4NET@gmail.com>
2022-08-30 09:35:38 -0300

    feat(mobile): singleton for api client

--- apps/mobile/lib/modules/core/core_module.dart
+++ apps/mobile/lib/modules/core/core_module.dart
@@ -2,6 +2,8 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:itourism_mobile/infra/notification/notification_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../infra/api/http/api_client.dart';
+
 class CoreModule extends Module {
   @override
   List<Bind> get binds => [
@@ -10,5 +12,6 @@ class CoreModule extends Module {
           export: true,
         ),
         Bind.lazySingleton((i) => NotificationService(), export: true),
+        Bind.lazySingleton((i) => ApiClient(), export: true),
       ];
 }
