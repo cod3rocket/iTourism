c86ab0033f8a3075bb4b7b9749e34eb1b29929e6
Henrique GC <henriq4NET@gmail.com>
2022-08-30 12:03:15 -0300

    fix(mobile): absolute import for api in core

--- apps/mobile/lib/modules/core/core_module.dart
+++ apps/mobile/lib/modules/core/core_module.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:itourism_mobile/infra/notification/notification_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import '../../infra/api/http/api_client.dart';
+import 'package:itourism_mobile/infra/api/http/api_client.dart';
+import 'package:itourism_mobile/infra/notification/notification_service.dart';
 
 class CoreModule extends Module {
   @override
