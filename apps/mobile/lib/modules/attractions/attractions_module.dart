1870a1f9bd0408cea9397223efc48da31f1dd024
Henrique GC <henriq4NET@gmail.com>
2022-09-03 02:11:18 -0300

    chore(mobile): update attractions module

--- apps/mobile/lib/modules/attractions/attractions_module.dart
+++ apps/mobile/lib/modules/attractions/attractions_module.dart
@@ -1,14 +1,27 @@
 import 'package:flutter_modular/flutter_modular.dart';
-
-import 'attractions_page.dart';
+import 'package:itourism_mobile/modules/attractions/controllers/attractions_controller.dart';
+import 'package:itourism_mobile/modules/attractions/pages/attractions_page.dart';
+import 'package:itourism_mobile/modules/attractions/repositories/attraction_repository.dart';
+import 'package:itourism_mobile/modules/attractions/pages/attraction_page.dart';
 
 class AttractionsModule extends Module {
+  @override
+  List<Bind> get binds => [
+        Bind((i) => AttractionsRepository(i())),
+        Bind((i) => AttractionsController(i())),
+      ];
+
   @override
   final List<ModularRoute> routes = [
     ChildRoute(
       '/',
       child: (_, args) => const AttractionsPage(),
       transition: TransitionType.fadeIn,
+    ),
+    ChildRoute(
+      '/:attractionId',
+      child: (_, args) =>
+          AttractionPage(attractionId: args.params['attractionId']),
     ),
   ];
 }
