8e7f357f9b9438fa232153fdbf4147840f45c761
Henrique GC <henriq4NET@gmail.com>
2022-09-03 02:09:10 -0300

    chore(mobile): create attractions controller

---
+++ apps/mobile/lib/modules/attractions/controllers/attractions_controller.dart
@@ -0,0 +1,36 @@
+import 'package:itourism_mobile/modules/attractions/models/attraction_model.dart';
+import 'package:itourism_mobile/modules/attractions/repositories/attraction_repository.dart';
+import 'package:mobx/mobx.dart';
+
+part 'attractions_controller.g.dart';
+
+class AttractionsController = _AttractionsControllerBase
+    with _$AttractionsController;
+
+abstract class _AttractionsControllerBase with Store {
+  final AttractionsRepository attractionsRepository;
+
+  _AttractionsControllerBase(this.attractionsRepository) {
+    fetchAttractions();
+  }
+
+  @observable
+  List<Attraction> attractions = [];
+
+  @computed
+  List<Attraction> get featuredAttractions => attractions
+      .where((attraction) => attraction.type == AttractionType.hotel)
+      .toList();
+
+  @observable
+  bool loading = false;
+
+  @action
+  Future<void> fetchAttractions() async {
+    loading = true;
+
+    attractions = await attractionsRepository.fetchAttractions();
+
+    loading = false;
+  }
+}
---
+++ apps/mobile/lib/modules/attractions/controllers/attractions_controller.g.dart
@@ -0,0 +1,69 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'attractions_controller.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$AttractionsController on _AttractionsControllerBase, Store {
+  Computed<List<Attraction>>? _$featuredAttractionsComputed;
+
+  @override
+  List<Attraction> get featuredAttractions => (_$featuredAttractionsComputed ??=
+          Computed<List<Attraction>>(() => super.featuredAttractions,
+              name: '_AttractionsControllerBase.featuredAttractions'))
+      .value;
+
+  late final _$attractionsAtom =
+      Atom(name: '_AttractionsControllerBase.attractions', context: context);
+
+  @override
+  List<Attraction> get attractions {
+    _$attractionsAtom.reportRead();
+    return super.attractions;
+  }
+
+  @override
+  set attractions(List<Attraction> value) {
+    _$attractionsAtom.reportWrite(value, super.attractions, () {
+      super.attractions = value;
+    });
+  }
+
+  late final _$loadingAtom =
+      Atom(name: '_AttractionsControllerBase.loading', context: context);
+
+  @override
+  bool get loading {
+    _$loadingAtom.reportRead();
+    return super.loading;
+  }
+
+  @override
+  set loading(bool value) {
+    _$loadingAtom.reportWrite(value, super.loading, () {
+      super.loading = value;
+    });
+  }
+
+  late final _$fetchAttractionsAsyncAction = AsyncAction(
+      '_AttractionsControllerBase.fetchAttractions',
+      context: context);
+
+  @override
+  Future<void> fetchAttractions() {
+    return _$fetchAttractionsAsyncAction.run(() => super.fetchAttractions());
+  }
+
+  @override
+  String toString() {
+    return '''
+attractions: ${attractions},
+loading: ${loading},
+featuredAttractions: ${featuredAttractions}
+    ''';
+  }
+}
