f45e46ad7f0335b371515a797456d05759f5d5fb
Henrique GC <henriq4NET@gmail.com>
2022-09-03 02:07:36 -0300

    chore(mobile): create list card view

---
+++ packages/starbase/lib/components/list_card/list_card.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class ListCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String imageUrl;
+  final num rate;
+  final Function()? onTap;
+
+  final BorderRadius _borderRadius = const BorderRadius.all(
+    Radius.circular(8),
+  );
+
+  const ListCard({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.imageUrl,
+    required this.rate,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _theme = Theme.of(context);
+
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: _borderRadius,
+        ),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+        height: 116,
+        child: ClipRRect(
+          clipBehavior: Clip.antiAlias,
+          borderRadius: _borderRadius,
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0.0,
+                top: 0.0,
+                bottom: 0.0,
+                child: ClipRRect(
+                  clipBehavior: Clip.antiAlias,
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(8),
+                    topLeft: Radius.circular(8),
+                  ),
+                  child: CachedNetworkImage(
+                    width: 130.0,
+                    imageUrl: imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(8),
+                      bottomRight: Radius.circular(8),
+                    ),
+                    color: _theme.colorScheme.primary.withOpacity(0.1),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 0,
+                left: 130,
+                bottom: 0,
+                right: 0,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ListTile(
+                        title: Text(title),
+                        subtitle: Text(subtitle, maxLines: 2),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 16,
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 16,
+                              color: _theme.iconTheme.color,
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              rate.toDouble().toString(),
+                              style: _theme.textTheme.bodyText2,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
