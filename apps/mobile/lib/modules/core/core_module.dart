b874b9605b033b5c4a162fd8293468cc05a8a628
Henrique GC <henriq4NET@gmail.com>
2022-09-04 12:47:01 -0300

    chore(mobile): inject async binds to cloud message service

--- apps/mobile/lib/modules/core/core_module.dart
+++ apps/mobile/lib/modules/core/core_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:itourism_mobile/infra/firebase/core.dart';
+import 'package:itourism_mobile/infra/firebase/cloud_message/cloud_message_service.dart';
+import 'package:itourism_mobile/infra/firebase/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:itourism_mobile/infra/api/http/api_client.dart';
 import 'package:itourism_mobile/infra/notification/notification_service.dart';
@@ -11,8 +12,15 @@ class CoreModule extends Module {
           (i) => SharedPreferences.getInstance(),
           export: true,
         ),
-        AsyncBind((i) => FirebaseCore.initialize()),
+        AsyncBind(
+          (i) => FirebaseCore.initialize(),
+          export: true,
+        ),
         Bind.lazySingleton((i) => NotificationService(), export: true),
+        AsyncBind(
+          (i) => CloudMessageService.initCloudMessageService(i()),
+          export: true,
+        ),
         Bind.lazySingleton((i) => ApiClient.createApi(), export: true),
       ];
 }
