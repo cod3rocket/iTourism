e0ba82ddd9ea3dfdf485ae2ec02f3d36b126b5e7
Henrique GC <henriq4NET@gmail.com>
2022-09-08 01:58:19 -0300

    chore(mobile): public utility repository

---
+++ apps/mobile/lib/modules/public_utility/repositories/public_utility_repository.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+
+import '../models/public_utility_model.dart';
+
+class PublicUtilityRepository {
+  final Dio apiClient;
+
+  PublicUtilityRepository(this.apiClient);
+
+  Future<List<PublicUtility>> findAll() async {
+    final data = [
+      {
+        'id': '1',
+        'name': 'Prefeitura Municipal',
+        'description': 'Prefeitura Municipal de Bandeirantes',
+        'latitude': 0.0,
+        'longitude': 0.0,
+        'tel': 4335422052,
+        'createdAt': '2021-07-01T00:00:00.000Z',
+        'updatedAt': '2021-07-01T00:00:00.000Z'
+      },
+      {
+        'id': '2',
+        'name': 'Conselho Tutelar',
+        'description': 'Conselho Tutelar de Bandeirantes',
+        'latitude': 0.0,
+        'longitude': 0.0,
+        'tel': 4335426582,
+        'createdAt': '2021-07-01T00:00:00.000Z',
+        'updatedAt': '2021-07-01T00:00:00.000Z'
+      },
+      {
+        'id': '3',
+        'name': 'Correios',
+        'description': 'Correios de Bandeirantes',
+        'latitude': 0.0,
+        'longitude': 0.0,
+        'tel': 4335423747,
+        'createdAt': '2021-07-01T00:00:00.000Z',
+        'updatedAt': '2021-07-01T00:00:00.000Z'
+      },
+      {
+        'id': '4',
+        'name': 'Posto de Saúde',
+        'description': 'Posto de Saúde de Bandeirantes',
+        'latitude': 0.0,
+        'longitude': 0.0,
+        'tel': 4335424980,
+        'createdAt': '2021-07-01T00:00:00.000Z',
+        'updatedAt': '2021-07-01T00:00:00.000Z'
+      },
+    ];
+
+    return data.map((e) => PublicUtility.fromJson(e)).toList();
+  }
+}
