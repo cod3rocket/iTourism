f13e57266487fc4c0efb1c075e830561fadf14b2
Eduardo Teles <edu.teles@pm.me>
2022-09-15 11:19:23 -0300

    feat(mobile): add i18n to home module

--- apps/mobile/lib/config/i18n.dart
+++ apps/mobile/lib/config/i18n.dart
@@ -1,1 +1,7 @@
-class I18nWords {}
+class I18nWords {
+  static String navigationBarItemAttractions =
+      'navigation-bar-item-attractions';
+  static String navigationBarItemEvents = 'navigation-bar-item-events';
+  static String navigationBarItemPublicUtility =
+      'navigation-bar-item-public-utility';
+}
