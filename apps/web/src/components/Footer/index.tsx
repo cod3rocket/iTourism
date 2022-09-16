4eebd635005d01616b44f8cabadba882f6b75ff3
Henrique GC <henriq4NET@gmail.com>
2022-09-16 12:40:33 -0300

    feat(web): create footer component

---
+++ apps/web/src/components/Footer/index.tsx
@@ -0,0 +1,45 @@
+import { RiHeartFill } from 'react-icons/ri';
+
+import { Center, Icon, Text, Link } from '@chakra-ui/react';
+
+function Footer() {
+  return (
+    <Center
+      display="flex"
+      as="footer"
+      w="full"
+      bg="blackAlpha.900"
+      p="8"
+      flexDir="column"
+      gap="2"
+    >
+      <Text color="white">
+        Proudly made with <Icon as={RiHeartFill} color="red.500" /> by{' '}
+        <Link href="https://github.com/henriq4" color="purple.200" isExternal>
+          henriq4
+        </Link>{' '}
+        and{' '}
+        <Link
+          href="https://github.com/eduardoteles17"
+          color="purple.200"
+          isExternal
+        >
+          eduardoteles17
+        </Link>
+      </Text>
+      <Text color="white">
+        © 2022{' '}
+        <Link
+          href="https://github.com/cod3rocket"
+          color="purple.200"
+          isExternal
+        >
+          Cod3Rocket
+        </Link>
+        . Todos os direitos reservados.
+      </Text>
+    </Center>
+  );
+}
+
+export default Footer;
