e56421f06b4c79d24eabce2ecb32fcfdc8d8d2d6
Eduardo Teles <edu.teles@pm.me>
2022-09-17 18:54:30 -0300

    feat(mobile): add select attraction type component

--- apps/mobile/lib/config/i18n.dart
+++ apps/mobile/lib/config/i18n.dart
@@ -16,6 +16,11 @@ class I18nWords {
   static String attractionsDetailsPageLocation =
       'attractions-details-page-location';
 
+  static String attractionTypeAttraction = 'attraction-type-attraction';
+  static String attractionTypeBars = 'attraction-type-bars';
+  static String attractionTypeHotel = 'attraction-type-hotel';
+  static String attractionTypeRestaurants = 'attraction-type-restaurants';
+
   // Events module
   static String eventsPageTitle = 'events-page-title';
   static String eventsPageNoData = 'events-page-no-data';
