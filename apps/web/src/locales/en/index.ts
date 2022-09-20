71095c6247998fde9edd80c67458948e08f26d80
Henrique GC <henriq4NET@gmail.com>
2022-09-19 23:02:48 -0300

    feat(web): implement locales languages - pt_br and en

---
+++ apps/web/src/locales/en/index.ts
@@ -0,0 +1,37 @@
+import { LocalesModel } from 'models/localesModel';
+
+const en: LocalesModel = {
+  header: {
+    contact: 'Contact',
+    features: 'Features',
+  },
+  footer: {
+    accomplishment: {
+      first: 'Proudly made with',
+      second: 'by',
+      third: 'and',
+    },
+    copyright: 'All rights reserved.',
+  },
+  features: {
+    title: {
+      utilities: 'Display city utilities',
+      extras: 'Extras',
+    },
+    attractions: 'Display attractions',
+    events: 'Display events',
+    public_utilities: 'Display public utilities',
+    real_time: 'Real-time informations',
+    push_notification: 'Push notification',
+    filter: 'Filter attractions by type',
+    maps: 'Google Maps integration',
+    lang_support: 'Viable in English and Portuguese',
+  },
+  heading: {
+    title: 'Discover the Pioneer North with iTourism',
+    subtitle:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi, cumque eos, culpa neque, reprehenderit assumenda atque explicabo molestiae ullam unde iste aut a consequatur nesciunt dolor commodi quod aspernatur.',
+  },
+};
+
+export { en };
---
+++ apps/web/src/locales/pt-br/index.ts
@@ -0,0 +1,37 @@
+import { LocalesModel } from 'models/localesModel';
+
+const pt_br: LocalesModel = {
+  header: {
+    contact: 'Contato',
+    features: 'Recursos',
+  },
+  footer: {
+    accomplishment: {
+      first: 'Orgulhosamente feito com',
+      second: 'por',
+      third: 'e',
+    },
+    copyright: 'Todos os direitos reservados.',
+  },
+  features: {
+    title: {
+      utilities: 'Exibição das utilidades da cidade',
+      extras: 'Extras',
+    },
+    attractions: 'Exibição das atrações',
+    events: 'Exibição dos eventos',
+    public_utilities: 'Exibição das utilidades públicas',
+    real_time: 'Informações atualizadas em tempo real',
+    push_notification: 'Notificações push',
+    filter: 'Filtro entre as principais atrações',
+    maps: 'Integração com Google Maps',
+    lang_support: 'Suporte para múltiplas linguagens',
+  },
+  heading: {
+    title: 'Descubra o Norte Pioneiro com iTourism',
+    subtitle:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi, cumque eos, culpa neque, reprehenderit assumenda atque explicabo molestiae ullam unde iste aut a consequatur nesciunt dolor commodi quod aspernatur.',
+  },
+};
+
+export { pt_br };
