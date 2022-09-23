a8de242a089ab9d89b6c8c636697eb40848aecc4
Henrique GC <henriq4NET@gmail.com>
2022-09-22 21:14:46 -0300

    feat(web): stylizyng footer - fixing color and texts

--- apps/web/src/components/Footer/index.tsx
+++ apps/web/src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { RiHeartFill } from 'react-icons/ri';
 
-import { Center, Icon, Text, Link } from '@chakra-ui/react';
+import { Center, Icon, Text, Link, Heading, Flex } from '@chakra-ui/react';
 import { useLang } from 'hooks/useLang';
 
 function Footer() {
@@ -9,36 +9,34 @@ function Footer() {
   return (
     <Center
       display="flex"
+      justifyContent="space-evenly"
       as="footer"
       w="full"
-      bg="blackAlpha.900"
+      bg="#3c3c4225"
       p="8"
       flexDir="column"
       gap="2"
+      minH="32"
     >
-      <Text color="white">
+      <Text color="#a8a8b3" fontSize="md" textAlign="center">
         {l.footer.accomplishment.first}{' '}
         <Icon as={RiHeartFill} color="red.500" />{' '}
         {l.footer.accomplishment.second}{' '}
-        <Link href="https://github.com/henriq4" color="purple.200" isExternal>
+        <Link href="https://github.com/henriq4" color="#edededdd" isExternal>
           henriq4
         </Link>{' '}
         {l.footer.accomplishment.third}{' '}
         <Link
           href="https://github.com/eduardoteles17"
-          color="purple.200"
+          color="#edededdd"
           isExternal
         >
           eduardoteles17
         </Link>
       </Text>
-      <Text color="white">
+      <Text mb="-4" fontSize="sm" color="#a8a8b3" textAlign="center">
         © 2022{' '}
-        <Link
-          href="https://github.com/cod3rocket"
-          color="purple.200"
-          isExternal
-        >
+        <Link href="https://github.com/cod3rocket" color="#edededdd" isExternal>
           Cod3Rocket
         </Link>
         . {l.footer.copyright}
