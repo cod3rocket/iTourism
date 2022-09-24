652332062efecc2b60087ad0bef3205bc42d4b6d
Henrique GC <henriq4NET@gmail.com>
2022-09-23 23:27:37 -0300

    feat(web): create responsive header drawer

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { HeaderDrawerContextProvider } from 'contexts/HeaderDrawerContext';
 import type { AppProps } from 'next/app';
 import { theme } from 'styles/themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <HeaderDrawerContextProvider>
+        <Component {...pageProps} />
+      </HeaderDrawerContextProvider>
     </ChakraProvider>
   );
 }
